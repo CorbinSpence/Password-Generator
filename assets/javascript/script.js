dcb221f9f8e5375824e39acf3450502760c05506
CorbinSpence <corbinspence16@gmail.com>
2023-02-08 14:40:27 -0500

    Added prompts for criteria

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,20 @@
 // Assignment code here
-
+var processLength = confirm("Add password length criteria");
+var processChars = confirm("Add password character criteria");
+if(processChars){
+  let selectedOption = false
+  while(!selectedOption){
+    var lowercase = confirm("Character criteria: include lowercase");
+    var uppercase = confirm("Character criteria: include uppercase");
+    var numbers = confirm("Character criteria: include numbers");
+    var special = confirm("Character criteria: include special characters");
+    if(lowercase || uppercase || numbers|| special){
+      selectedOption = true
+    }else{
+      let errorMessage = alert("Choose at least one option")
+    }
+  }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
